afe859987e2067f6f4bc3baebb6f6eb53b42d287
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-02 19:03:26 +0200

    Overloading

--- Demo OOP Session 3/Program.cs
+++ Demo OOP Session 3/Program.cs
@@ -2,11 +2,36 @@
 {
     internal class Program
     {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Sum(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        public static double Sum(double x, int y)
+        {
+            return x + y;
+        }
+        public static double Sum(int x, double y)
+        {
+            return x + y;
+        }
         static void Main(string[] args)
         {
 
             // poly Morphism
+            #region Overloading
             // 1. Overloading - 2. Overriding
+            // double result = Sum(1.5,2);
+            // Console.WriteLine(result);
+            //result = Sum3(5, 6, 7);
+            Console.WriteLine('n');
+            //Console.WriteLineint();
+            //Console.WriteLinestring();
+            //Console.WriteLinechar();
+            #endregion
         }
     }
 }
