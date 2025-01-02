076669a71646e68cdcdef53955c77ee4b53df4a2
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-02 18:51:49 +0200

    Add project files.

---
+++ Demo OOP Session 3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo_OOP_Session_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // poly Morphism
+            // 1. Overloading - 2. Overriding
+        }
+    }
+}
